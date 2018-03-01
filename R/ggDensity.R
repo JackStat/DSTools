f9784f598cb2b7029ab28f29e00c0bec1eb7de70
Tyler Hunt <tyler.hunt@utah.edu>
2018-02-28 19:25:15 -0700

    adding function for generated density plot with a filter

---
+++ R/ggDensity.R
@@ -0,0 +1,30 @@
+#' @title Apply a filter to a data.frame
+#'
+#' @param df data.frame
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @export
+
+ggDensity <- function(df = mtcars, filter.var = 'cyl', filter.val = 6, variable = 'wt'){
+
+  if('factor' %in% class(df[,filter.var])){
+    Filter <- paste0(filter.var, " == '", filter.val, "'")
+  } else {
+    Filter <- paste0(filter.var, " == ", filter.val)
+  }
+
+
+  # print(Filter)
+
+  df2 <<-
+    df %>%
+    filter_(Filter)
+
+  # print(df2)
+  ggplot(df2, aes_string(x = noquote(variable))) + geom_density()
+
+}
+
+
