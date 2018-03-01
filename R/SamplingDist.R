56202422a114ebfadab2a665b1a62277bb502410
Tyler Hunt <tyler.hunt@utah.edu>
2018-02-28 19:01:03 -0700

    added error messages with tests

--- R/SamplingDist.R
+++ R/SamplingDist.R
@@ -8,6 +8,13 @@
 #' @export
 
 SamplingDist <- function(samples = 50, sample_size = 100, mean = 0, sd = 1){
+  if(samples <= 0){
+    stop("The number of samples needs to be greater than 0")
+  }
+  if(sd <= 0){
+    stop("The standard devaition cannot be != 0")
+  }
+
   sampleMeans <- rep(NA, samples)
   for(i in 1:samples){
     x <- rnorm(sample_size, mean = mean, sd = sd)
--- tests/testthat/test_SamplingDist.R
+++ tests/testthat/test_SamplingDist.R
@@ -10,3 +10,15 @@ test_that("Devations from population mean are extreme", {
   ss <- SamplingDist(samples = 5)
   expect_true(all(abs(ss) <= 5))
 })
+
+
+
+test_that("Error messages are not working properly",{
+  testthat::expect_error(SamplingDist(0)
+    , "The number of samples needs to be greater than 0")
+
+  testthat::expect_error(SamplingDist(sd = 0)
+    , "The standard devaition cannot be != 0")
+})
+
+
